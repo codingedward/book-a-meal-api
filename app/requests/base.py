e509045100ac5e28f9eea7e4380fa53ad9e23f09
Edward Njoroge <codingedward@gmail.com>
2018-06-21 12:24:43 +0300

    Add base form request

---
+++ app/requests/base.py
@@ -0,0 +1,20 @@
+from flask import request
+from app.validation.validator import Validator, ValidationException
+
+
+class JsonRequest:
+
+    def __init__(self):
+        if not request.is_json:
+            raise ValidationException(['Request must be valid JSON'])
+        self.validator = Validator(
+            rules=self.rules(),
+            request=request.json
+        )
+
+    def validate(self):
+        if self.validator.fails():
+            raise ValidationException(self.validator.first())
+
+    def rules(self):
+        return {}
