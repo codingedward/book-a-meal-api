12c22e00957dbdcc322314eacc576cbd114e5c8c
Edward Njoroge <codingedward@gmail.com>
2018-06-21 13:05:13 +0300

    Add application factory method

---
+++ app/__init__.py
@@ -0,0 +1,40 @@
+"""Creates and configures an application"""
+
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from instance.config import app_config
+
+
+db = SQLAlchemy()
+
+
+from app.blueprints.auth import auth
+from app.exceptions import handler
+
+
+def create_app(config_name):
+    """This will create the application and setup all the 
+    other extensions"""
+
+    # initialize flask and jwt
+    app = Flask(__name__, instance_relative_config=True)
+    jwt = JWTManager(app)
+
+    # load configuration
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+
+    # register blueprints
+    app.register_blueprint(auth)
+    app.register_blueprint(documentation)
+
+    # initialize the database
+    db.init_app(app)
+    # application exceptions handler
+    handler.init_app(app)
+    # jwt blacklists handler
+    handler.init_jwt(jwt)
+
+    return app
