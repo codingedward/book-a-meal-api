5e09651230f42ef751feb8c05f6993e734295bae
Edward Njoroge <codingedward@gmail.com>
2018-06-22 23:21:57 +0300

    Add base json request

--- app/requests/base.py
+++ app/requests/base.py
@@ -1,3 +1,4 @@
+import re
 from flask import request
 from app.validation.validator import Validator, ValidationException
 
@@ -6,12 +7,17 @@ class JsonRequest:
     def __init__(self):
         if not request.is_json:
             raise ValidationException(['Request must be valid JSON'])
+        for field, value in request.json.items():
+            
+            if isinstance(value, str):
+                request.json[field] = re.sub('\s+', ' ', value).strip()
         self.validator = Validator(
             rules=self.rules(),
             request=request.json
         )
 
     def validate(self):
+        str(self.validator.errors())
         if self.validator.fails():
             raise ValidationException(self.validator.errors())
 
