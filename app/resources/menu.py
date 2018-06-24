07beb558dfa9c151ebb5235e1c10964ef9ac43ec
Edward Njoroge <codingedward@gmail.com>
2018-06-24 11:21:42 +0300

    Add menu resource controller

---
+++ app/resources/menu.py
@@ -0,0 +1,96 @@
+from flask import request
+from app.models import Menu
+from flask_restful import Resource
+from app.validation import validate
+from app.requests.menu import PostRequest, PutRequest
+from app.middlewares.auth import user_auth, admin_auth
+
+
+class MenuResource(Resource):
+
+    @user_auth
+    def get(self, menu_id):
+        # exists? ...
+        menu = Menu.query.get(menu_id)
+        if not menu:
+            return {
+                'success': False,
+                'message': 'Menu not found',
+            }, 404
+
+        return {
+            'success': True,
+            'message': 'Menu successfully retrieved',
+            'menu': menu.to_dict()
+        }
+
+    @admin_auth
+    @validate(PutRequest)
+    def put(self, menu_id):
+
+        # check if another menu exists with new name...
+        if request.json.get('name'):
+            name = request.json['name']
+            menu = Menu.query.filter_by(name=name).first()
+            if menu and menu.name.lower() == name.lower() and \
+                    menu.id != menu_id:
+                return {
+                    'success': False,
+                    'message': 'Validation error',
+                    'errors': {
+                        'name': ['Menu name must be unique']
+                    }
+                }, 400
+
+        # check exists? ...
+        menu = Menu.query.get(menu_id)
+        if not menu:
+            return {
+                'success': False,
+                'message': 'Menu not found',
+            }, 404
+
+        # now update...
+        menu.update(request.json)
+        return {
+            'success': True,
+            'message': 'Menu successfully updated',
+            'menu': menu.to_dict()
+        }
+
+    @admin_auth
+    def delete(self, menu_id):
+        # exists? ...
+        menu = Menu.query.get(menu_id)
+        if not menu:
+            return {
+                'success': False,
+                'message': 'Menu not found',
+            }, 404
+
+        menu.delete()
+        return {
+            'success': True,
+            'message': 'Menu successfully deleted',
+        }
+
+
+class MenuListResource(Resource):
+    @user_auth
+    def get(self):
+        resp = Menu.paginate()
+        resp['menus'] = resp['data']
+        resp['message'] = 'Successfully retrieved menus'
+        resp['success'] = True,
+        return resp
+
+    @admin_auth
+    @validate(PostRequest)
+    def post(self):
+        menu = Menu.create(request.json)
+        return {
+            'success': True,
+            'message': 'Successfully saved menu.',
+            'menu': menu.to_dict()
+        }, 201
+
