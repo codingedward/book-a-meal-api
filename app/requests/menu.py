c7c962f3fbaa4113a2d95a85b94e5f5f59e31abf
Edward Njoroge <codingedward@gmail.com>
2018-06-30 16:31:29 +0300

    Fix menu name to be letters only

--- app/requests/menu.py
+++ app/requests/menu.py
@@ -4,12 +4,12 @@ from .base import JsonRequest
 class PostRequest(JsonRequest):
     def rules(self):
         return {
-            'name': 'required|string|unique:Menu,name',
+            'name': 'required|alpha|unique:Menu,name',
         }
 
 
 class PutRequest(JsonRequest):
     def rules(self):
         return {
-            'name': 'required|string',
+            'name': 'required|alpha',
         }
