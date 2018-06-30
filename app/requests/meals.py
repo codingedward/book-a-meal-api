56be55407bd28634d4f98bc59686b001db19d251
Edward Njoroge <codingedward@gmail.com>
2018-06-30 16:30:29 +0300

    Fix meal name to be letters only

--- app/requests/meals.py
+++ app/requests/meals.py
@@ -4,7 +4,7 @@ from .base import JsonRequest
 class PostRequest(JsonRequest):
     def rules(self):
         return {
-            'name': 'required|string|unique:Meal,name',
+            'name': 'required|alpha|unique:Meal,name',
             'cost': 'required|positive',
             'img_url': 'url',
         }
@@ -13,7 +13,7 @@ class PostRequest(JsonRequest):
 class PutRequest(JsonRequest):
     def rules(self):
         return {
-            'name': 'string',
+            'name': 'alpha',
             'cost': 'positive',
             'img_url': 'url',
         }
